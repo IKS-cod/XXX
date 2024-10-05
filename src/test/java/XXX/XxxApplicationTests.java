a198574b0138822316f8205af5c67dce3bf20015
IKS-cod <ik.smirnov@yandex.ru>
2024-10-05 11:39:24 +0300

    Initial commit

---
+++ src/test/java/XXX/XxxApplicationTests.java
@@ -0,0 +1,13 @@
+package XXX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
